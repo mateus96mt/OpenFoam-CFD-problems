37a00cfd88de718387afff8bb9511ae4c38b13e0
Mateus Teixeira Magalhães <mateus.teixeira@haizen.tech>
2021-05-20 12:05:08 -0300

    Calculo de erro ente numerica e experimental

--- cavity/cavity/read_vtk.py
+++ cavity/cavity/read_vtk.py
@@ -66,7 +66,7 @@ def velAtCenter(fileName = 'VTK/linear_38000.vtk'):
         
         count = count + 1
     
-    print(len(vel_x_center))
+#    print(len(vel_x_center))
     
     #print("\n\nvel_x:\n", vel_x)
     #print("\n\nvel_x:\n", vel_y)
@@ -76,55 +76,94 @@ def velAtCenter(fileName = 'VTK/linear_38000.vtk'):
      
     return x_axes, y_axes, vel_x_center, vel_y_center
 
-#scheme_nameS = ['linear', 'TOPUS', 'FSFL', 'SDPUS', 'EPUS']
-scheme_nameS = ['TOPUS', 'FSFL', 'SDPUS', 'EPUS']
-ReS = [1000]
-
-plt.figure(figsize=(8,8))
-
-label_font_size = 12
-
-for scheme_name in scheme_nameS:
-
-    for Re in ReS:
+def error(y_num, y_referencia):
     
-        path = 'Reynolds/' + str(Re) + '/' + scheme_name
+    n = len(y_referencia)
+    m = len(y_num)
+        
+    erro = 0.0
+    sum_exata = 0.0
     
-        if scheme_name == 'linear':
+    for i in range(n):
+
+        er_min = 999999999999
+
+        for j in range(m):
             
-            path = 'linear'
+            er = (y_referencia[i] - y_num[j])**2
+            
+            if er < er_min:
+                
+                er_min = er
+        
+        sum_exata = sum_exata + (y_referencia[i]**2)
+        erro = erro + er_min
+        
+    return np.sqrt(erro/sum_exata)
+
+def run():
+    #scheme_nameS = ['linear', 'TOPUS', 'FSFL', 'SDPUS', 'EPUS']
+    scheme_nameS = ['TOPUS', 'FSFL', 'SDPUS', 'EPUS']
+#    scheme_nameS = ['linear']
+    ReS = [1000, 7500]
     
-        x_axes, y_axes, vel_x_center, vel_y_center = velAtCenter(fileName = path + '/VTK/' + scheme_name + '_38000.vtk')
+    plt.figure(figsize=(8,8))
+    
+    label_font_size = 12
+    
+    for scheme_name in scheme_nameS:
         
-        x_axes_ref, vel_U_ref = readOutPut('referencia/referencia_U_Re_' + str(Re) + '_128x128.txt')
-        x_axes_ref, vel_V_ref = readOutPut('referencia/referencia_V_Re_' + str(Re) + '_128x128.txt')
+        print("\n\nscheme_name = " + scheme_name)
         
+        for Re in ReS:
         
-        plt.plot(y_axes, vel_x_center, label = 'Solução numérica', marker = '+')
-        plt.plot(x_axes_ref, vel_U_ref, 'v', label = 'Dados experimentais')
-        plt.grid(True, linestyle='--')
-        plt.title(scheme_name)
-        plt.legend(loc="best")
-        plt.xlabel(r'$y$', fontsize=label_font_size)
-        plt.ylabel(r'$U_x$', fontsize=label_font_size, rotation=0)
-        plt.tight_layout()
-        plt.savefig(scheme_name + '_Re=' + str(Re) + '_vel_x' + '.png', dpi = 200)
-        plt.cla()
-        plt.clf()
+            path = 'Reynolds/' + str(Re) + '/' + scheme_name
         
-        plt.plot(x_axes, vel_y_center, label = 'Solução numérica', marker = '+')
-        plt.plot(x_axes_ref, vel_V_ref, 'v', label = 'Dados experimentais')
-        plt.grid(True, linestyle='--')
-        plt.title(scheme_name)
-        plt.legend(loc="best")
-        plt.xlabel(r'$x$', fontsize=label_font_size)
-        plt.ylabel(r'$U_y$', fontsize=label_font_size, rotation=0)
-        plt.tight_layout()
-        plt.savefig(scheme_name + '_Re=' + str(Re) + '_vel_y' + '.png', dpi = 200)
-        plt.cla()
-        plt.clf()
+            if scheme_name == 'linear':
+                
+                path = 'linear'
+        
+            x_axes, y_axes, vel_x_center, vel_y_center = velAtCenter(fileName = path + '/VTK/' + scheme_name + '_38000.vtk')
+            
+            y_axes_ref, vel_U_ref = readOutPut('referencia/referencia_U_Re_' + str(Re) + '_128x128.txt')
+            x_axes_ref, vel_V_ref = readOutPut('referencia/referencia_V_Re_' + str(Re) + '_128x128.txt')
+            
+            
+            plt.plot(y_axes, vel_x_center, label = 'Solução numérica', marker = '+')
+            plt.plot(y_axes_ref, vel_U_ref, 'v', label = 'Referência')
+            plt.grid(True, linestyle='--')
+#            plt.title(scheme_name)
+            plt.legend(loc="best")
+            plt.xlabel(r'$y$', fontsize=label_font_size)
+            plt.ylabel(r'$U_x$', fontsize=label_font_size, rotation=0)
+            plt.tight_layout()
+            plt.savefig(scheme_name + '_Re=' + str(Re) + '_vel_x' + '.png', dpi = 200)
+            plt.cla()
+            plt.clf()
+            
+            plt.plot(x_axes, vel_y_center, label = 'Solução numérica', marker = '+')
+            plt.plot(x_axes_ref, vel_V_ref, 'v', label = 'Referência')
+            plt.grid(True, linestyle='--')
+#            plt.title(scheme_name)
+            plt.legend(loc="best")
+            plt.xlabel(r'$x$', fontsize=label_font_size)
+            plt.ylabel(r'$U_y$', fontsize=label_font_size, rotation=0)
+            plt.tight_layout()
+            plt.savefig(scheme_name + '_Re=' + str(Re) + '_vel_y' + '.png', dpi = 200)
+            plt.cla()
+            plt.clf()
+
+            print("Re = " + str(Re))
+            print("erro Ux: " + str("%.5f" % error(vel_x_center, vel_U_ref)))
+            print("erro Uy: " + str("%.5f" % error(vel_y_center, vel_V_ref)) + "\n")
+            
 
 
+run()
+
+#x_axes_ref, vel_U_ref = readOutPut('referencia/referencia_U_Re_1000_128x128.txt')
+#
+#plt.plot(x_axes_ref, vel_U_ref)
 
 ### get a field in numpy array
 #p_cell = grid.cell_arrays['p']
